ae2d6f549849f136b98a54557e4630498476b9eb
krau <krau39@outlook.com>
2023-01-26 22:24:44 +0800

    删除testlog函数

--- main.go
+++ main.go
@@ -2,19 +2,8 @@ package main
 
 import (
 	"github.com/krau/favpics-helper/cmd"
-	"github.com/krau/favpics-helper/pkg/util"
 )
 
 func main() {
 	cmd.CycleTask()
-}
-
-func testlog() {
-	util.Log.Trace("this is a Trace log message")
-	util.Log.Debug("this is a Debug log message")
-	util.Log.Info("this is a Info log message")
-	util.Log.Warn("this is a Warn log message")
-	util.Log.Error("this is a Error log message")
-	util.Log.Fatal("this is a Fatal log message")
-	util.Log.Panic("this is a Panic log message")
 }
