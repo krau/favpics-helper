0936559cbd9f55eaa5c57616a3e16f9ec6a4e5d7
krau <krau39@outlook.com>
2023-01-27 22:11:58 +0800

    fix build

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-OS = $(shell uname -s)
-ARCH = $(shell uname -m)
+OS=$(uname -s)
+ARCH=$(uname -m)
 
 mkdir -p build
 go build -o build/ ./main.go
 cp config.toml build/
 tar -czvf favpics-helper-$(OS)-$(ARCH).tar.gz build/
+rm -rf build
+mkdir -p build
+mv favpics-helper-$(OS)-$(ARCH).tar.gz build/
